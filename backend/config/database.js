bdcc38ab8bc20a5bbc099056c3ae17906d54a3ab
Sara <ariadnas968@gmail.com>
2025-05-26 16:46:15 -0600

    conecte todas las vistas con el css, agrege las imagenes de logo y la de usuario, distingui nombres este ususario y admin y creo q ya

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,11 @@
-// config/database.js
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'puntodeventa', // ⚠️ nombre correcto de tu base de datos
+  database: 'puntodeventa', 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
